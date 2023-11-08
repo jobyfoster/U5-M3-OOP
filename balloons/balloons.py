373089c604f5a5ae9f95dfe105b325db859f986a
joby foster <jobyjfoster@gmail.com>
2023-11-08 11:14:55 -0600

    fixed

--- balloons/balloons.py
+++ balloons/balloons.py
@@ -3,6 +3,10 @@ class BalloonTooFull(Exception):
 
 
 class Balloon:
+    def __init__(self):
+        self.capacity = 5
+        self.amount = 0
+
     def pump(self):
         self.amount += 3
         if self.amount > self.capacity:
@@ -11,3 +15,20 @@ class Balloon:
     def release(self):
         self.amount -= 2
         self.amount = max(0, self.amount)
+
+
+class SwordBalloon(Balloon):
+    def __init__(self):
+        super().__init__()
+
+
+class DogBalloon(Balloon):
+    def __init__(self):
+        super().__init__()
+        self.capacity = 7
+
+
+class TriforceBalloon(Balloon):
+    def __init__(self):
+        super().__init__()
+        self.capacity = 11
